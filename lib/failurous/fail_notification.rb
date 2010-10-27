1837b622f97ee2c5f2e91e3bda2a4de6f9ee27f0
Mikko Nylén <mikko.nylen@gmail.com>
2010-10-27 19:29:30 +0300

    Removed the stub for ignore? method

--- lib/failurous/fail_notification.rb
+++ lib/failurous/fail_notification.rb
@@ -28,16 +28,6 @@ module Failurous
       fill_from_object(object) if object
     end
 
-    # Determines whether FailMiddleware should ignore _exception_ in _object_.
-    # Default implementation returns *false* for everything.
-    #
-    # @param exception [Exception] the exception
-    # @param object [Object] the object
-    # @return [Boolean] *true* if the exception in object should be ignored; *false* otherwise
-    def self.ignore?(exception, object = nil)
-
-    end
-
     # Fills notification details from the given exception. Please note that
     # if the title and/or location has been specified previously, those
     # are not overriden here.
