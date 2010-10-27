f5aee94e663cc7c4ec408cdc486ed3b34f040c45
Mikko Nylén <mikko.nylen@gmail.com>
2010-10-27 17:33:47 +0300

    Configuring the notifier

--- lib/failurous.rb
+++ lib/failurous.rb
@@ -1,7 +1,20 @@
+require 'net/http'
+require 'net/https'
+
 module Failurous
   ROOT = File.dirname(__FILE__)
   
   autoload :Config,           "#{ROOT}/failurous/config"
   autoload :FailNotification, "#{ROOT}/failurous/fail_notification"
   autoload :FailNotifier,     "#{ROOT}/failurous/fail_notifier"
+  
+  
+  def self.configure(&block)
+    block.call(Config)
+    
+    # Initialize the HTTP object for sending fails
+    FailNotifier.http = ::Net::HTTP.new(Config.server_name, Config.server_port)
+    FailNotifier.http.use_ssl = Config.use_ssl || false
+    FailNotifier.http.open_timeout = Config.send_timeout || 2
+  end
 end
