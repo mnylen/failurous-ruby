ae7dabd027aec42606f7861144d4778073e4837d
Mikko Nylén <mikko.nylen@gmail.com>
2010-10-28 20:03:29 +0300

    Documented configuration options

--- lib/failurous/config.rb
+++ lib/failurous/config.rb
@@ -1,11 +1,24 @@
 module Failurous
+  # Stores configuration for FailNotifier
   class Config
+    # Project's API key in Failurous
     attr_accessor :api_key
+    
+    # Server name
     attr_accessor :server_name
+    
+    # Server port (defaults to 80)
     attr_accessor :server_port
+    
+    # Timeout for sending fails, in seconds (defaults to 2 seconds)
     attr_accessor :send_timeout
+    
+    # Use SSL? Failurous server must be able to receive fails over HTTPS when this is set to true (defaults to false)
     attr_accessor :use_ssl
+    
+    # Logger instance for logging errors 
     attr_accessor :logger
+    
     
     def initialize
       @server_port  = 80
